8869e2722b7c421d176f030177f3714b9ab7c572
Deyvid Nascimento <deyvid.nascimento0101@gmail.com>
2020-10-27 09:56:05 -0300

    Middleware and Changing assets

--- level-01/01-backend-with-node/src/index.js
+++ level-01/01-backend-with-node/src/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { uuid } = require('uuidv4')
+const { uuid, isUuid } = require('uuidv4')
 
 const app = express()
 
@@ -23,7 +23,38 @@ app.use(express.json())
   * Request Body: Conteúdo na hora de criar ou editar um recurso (JSON)
   */
 
+ /**
+  * Middleware:
+  * 
+  * Interceptador de requisições de pode interromper totalmente a requisição ou alterar dados da requisição
+  */
+
 const projects = [] 
+
+function logRequests(request, response, next) {
+    const { method, url } = request
+
+    const logLabel = `[${method.toUpperCase()} ${url}]`
+
+    console.time(logLabel)
+
+    next() // Próximo middleware
+
+    console.timeEnd(logLabel)
+}
+
+function validateProjectId(request, response, next) {
+    const { id } = request.params
+
+    if (!isUuid(id)) {
+        return response.status(400).json({ error: "Invalid project ID." })
+    }
+    
+    return next()
+}
+
+app.use(logRequests)
+app.use('/projects/:id', validateProjectId)
 
 app.get('/projects', (request, response) => {
     const { title } = request.query
@@ -45,7 +76,7 @@ app.post('/projects', (request, response) => {
     return response.json(project)
 })
 
-app.put('/projects/:id', (request, response) => {
+app.put('/projects/:id', /*validateProjectId,*/ (request, response) => {
     const { id } = request.params
     const { title, owner } = request.body
     
