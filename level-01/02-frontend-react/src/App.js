da2988bb4cfcc820b80d186c32630dc27db042c7
Deyvid Nascimento <deyvid.nascimento0101@gmail.com>
2020-10-29 14:11:54 -0300

    Properties

--- level-01/02-frontend-react/src/App.js
+++ level-01/02-frontend-react/src/App.js
@@ -2,12 +2,30 @@ import React from 'react'
 
 import Header from './components/Header'
 
+/**
+ * Componentes;
+ * Propriedade;
+ * Estado
+ */
+
 function App() {
     return (
         // Fragment: Quando vocẽ precisa agrupar o componente, porém não deseja sujar a DOM.
         <>
-            <Header />
-            <Header />
+            <Header title="Homepage">
+                <ul>
+                    <li>Homepage</li>
+                    <li>Projects</li>
+                </ul>
+            </Header>
+
+            <Header title="Projects">
+                <ul>
+                    <li>Homepage</li>
+                    <li>Projects</li>
+                    <li>Login</li>
+                </ul>
+            </Header>
         </>
        
     )
