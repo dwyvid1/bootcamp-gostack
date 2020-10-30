b356b5a8ad5b8a116277c1a90009791868b53444
Deyvid Nascimento <deyvid.nascimento0101@gmail.com>
2020-10-30 16:20:41 -0300

    Listing projects and connecting to the database.

--- level-01/02-frontend-react/src/App.js
+++ level-01/02-frontend-react/src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import api from './services/api'
 
 import './App.css'
-import backgroundImage from './assets/background.jpeg'
 
 import Header from './components/Header'
 
@@ -21,7 +21,13 @@ import Header from './components/Header'
 // ...projects = copiando o que está em projects.
 
 function App() {
-    const [projects, setProjects] = useState(['Desenvolvimento de App', 'Desenvolvimento Front-end'])
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data)
+        })
+    }, [])
 
     function handleAddProject() {
         // projects.push(`Novo projeto ${Date.now()}`)
@@ -33,10 +39,8 @@ function App() {
         <>
             <Header title="Projects" />
 
-            <img width={300} src={backgroundImage}/>
-
             <ul>
-                {projects.map(project => <li key={project}>{project}</li>)}
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
             </ul>
 
             <button type="button" onClick={handleAddProject}>Adicionar</button>
