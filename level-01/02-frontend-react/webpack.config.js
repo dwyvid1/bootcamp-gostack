6b55fa8d9f67fcca4209935b3f3a5973f909c5ff
Deyvid Nascimento <deyvid.nascimento0101@gmail.com>
2020-10-30 11:56:40 -0300

    Importing CSS and images

--- level-01/02-frontend-react/webpack.config.js
+++ level-01/02-frontend-react/webpack.config.js
@@ -1,26 +1,37 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-    // Arquivo de entrada
-    entry: path.resolve(__dirname, 'src', 'index.js'),
-    // Arquivo gerado depois de ser convertido
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, 'public')
-    },
-    module: {
-        rules: [
-            {
-                // Converter arquivos JavaScript com Babel
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use : {
-                    loader: 'babel-loader',
-                }
-            }
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
         ]
-    }
-}
+      },
+      {
+        test: /.*\.(gif|png|jpe?g)$/i,
+        use: {
+          loader: 'file-loader',
+        }
+      }
+    ]
+  },
+};
