a7e544acef91ccb435b04daffd8e4aa8162fbb3e
Deyvid Nascimento <deyvid.nascimento0101@gmail.com>
2020-10-31 14:01:44 -0300

    Project Registration

--- level-01/02-frontend-react/src/App.js
+++ level-01/02-frontend-react/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+
 import api from './services/api'
 
 import './App.css'
@@ -29,10 +30,17 @@ function App() {
         })
     }, [])
 
-    function handleAddProject() {
-        // projects.push(`Novo projeto ${Date.now()}`)
+    async function handleAddProject() {
+        // setProjects([...projects, `Novo projeto ${Date.now()}`])
 
-        setProjects([...projects, `Novo projeto ${Date.now()}`])
+        const response = await api.post('projects', {
+            title: `Novo projeto ${Date.now()}`,
+            owner: "Deyvid Nascimento"
+        })
+
+        const project = response.data
+
+        setProjects([...projects, project])
     }
 
     return (
