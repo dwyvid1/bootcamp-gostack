b86ef8478bccf650956b8f61c49c6feec351f2c4
Deyvid Nascimento <deyvid.nascimento0101@gmail.com>
2020-11-06 13:06:21 -0300

    Creating new projects.

--- level-01/03-react-native/mobile/src/index.js
+++ level-01/03-react-native/mobile/src/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
+
+import api from './services/api'
 
 // Não possuem valor semântico (significado)
 // Não possuem estilização própria
@@ -9,12 +11,54 @@ import { View, Text, StyleSheet, StatusBar } from 'react-native';
 // Text: p, span, strong, h1, h2, h3
 
 export default function App() {
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data)
+        })
+    }, [])
+
+    async function handleAddProject() {
+        const response = await api.post('projects', {
+            title: `Novo Projeto ${Date.now()}`,
+            owner: 'Deyvid Nascimento'
+        })
+
+        const project = response.data
+
+        setProjects([... projects, project ])
+    }
+
     return (
         <>
-          <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-          <View style={styles.container}>
-            <Text style={styles.title}>Hello GoStack</Text>
-          </View>
+            <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+
+            {/**
+             FlatList: Mais performance com grnades listas
+
+             ScrolView: Não tem diferença visual
+            */}
+
+
+            <SafeAreaView style={styles.container}>
+            <FlatList 
+                data={projects}
+                keyExtractor={project => project.id}
+                renderItem={({ item: project }) => (
+                    <Text style={styles.project}>{project.title}</Text>
+                )}
+            />
+
+                <TouchableOpacity 
+                    activeOpacity={0.6}
+                    style={styles.button}
+                    onPress={handleAddProject}
+                >
+                    
+                    <Text style={styles.buttonText}>Adicionar projeto</Text>
+                </TouchableOpacity>
+            </SafeAreaView>
         </>      
     )
 }
@@ -23,14 +67,25 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#7159c1',
-      justifyContent: 'center',
-      alignItems: 'center'
     },
   
-    title: {
+    project: {
       color: '#fff',
-      fontSize: 32,
-      fontWeight: 'bold'
+      fontSize: 30,
+    },
+
+    button: {
+        backgroundColor: '#FFF',
+        margin: 20,
+        height: 50,
+        borderRadius: 4,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    buttonText: {
+        fontWeight: 'bold',
+        fontSize: 16,
     },
   })
     
