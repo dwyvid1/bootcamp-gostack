c7ff4e9cb2c8fa3f8da35b6ec04198a734b16356
Deyvid Nascimento <deyvid.nascimento0101@gmail.com>
2020-11-09 13:51:39 -0300

    Setting design pattern with ESLint and Prettier

--- level-02/01-first-nodejs-project/src/server.ts
+++ level-02/01-first-nodejs-project/src/server.ts
@@ -1,11 +1,15 @@
-import express, { response } from 'express'
+/* eslint-disable no-shadow */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-console */
+import express, { response } from 'express';
+import routes from './routes';
 
-const app = express()
+const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello GoStack' })
-})
+app.get('/', (request, response) =>
+    response.json({ message: 'Hello GoStack' }),
+);
 
 app.listen(3333, () => {
-    console.log('Server started on port 3333!')
-})
+    console.log('Server started on port 3333!');
+});
